21afcd25298124745be23af43312e62828e2f58e
Filippos <filipposlymperopoulos@filipposs-mbp.olin.edu>
2014-04-13 15:45:18 -0400

    Added pygame window basic.

--- Main.py
+++ Main.py
@@ -1,9 +1,17 @@
-import pyglet
- 
-win = pyglet.window.Window()
- 
-@win.event
-def on_draw():
-    win.clear()
- 
-pyglet.app.run()		
+import pygame
+
+background_colour = (255,255,255)
+(width, height) = (300, 200)
+
+screen = pygame.display.set_mode((width, height))
+
+pygame.display.set_caption('Shooter Platform')
+
+screen.fill(background_colour)
+
+pygame.display.flip()
+
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+    	False
