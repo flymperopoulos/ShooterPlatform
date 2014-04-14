ce4fa1871247089c0e118695bf61a8779d5921ef
Filippos <filipposlymperopoulos@Filipposs-MacBook-Pro.local>
2014-04-14 16:57:32 -0400

    Additions in openCV.

--- video.py
+++ video.py
@@ -4,8 +4,8 @@ import numpy as np
 import cv2
 
 cam = cv2.VideoCapture(0)
-#print cap.get(3)
-#print cap.get(4)
+print cam.get(3)
+print cam.get(4)
 
 green = np.uint8([[[0,0,255]]]) 
 hsv_green = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
@@ -20,15 +20,15 @@ while(True):
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
-#    hsv_frame = cv2.createImage(cv2.GetSize(frame), 8, 3)
+#   hsv_frame = cv2.createImage(cv2.GetSize(frame), 8, 3)
 
     # define range of blue color in HSV
     lower_blue = np.uint8([110, 100, 100])
-    upper_blue = np.uint8([170,255,204])
+    upper_blue = np.uint8([130,255,255])
 
     # define range of green color in HSV
-    lower_green = np.uint8([30, 100, 30])
-    upper_green = np.uint8([70, 255, 255])
+    lower_green = np.uint8([0, 255, 127])
+    upper_green = np.uint8([34, 139, 34])
 
     # Threshold the HSV image to get only blue colors
     blue = cv2.inRange(hsv, lower_blue, upper_blue)
@@ -36,16 +36,16 @@ while(True):
     green = cv2.inRange(hsv, lower_green, upper_green)
 
     moment = cv2.moments(blue)
-
+    
     moment1 = cv2.moments(green)
 
     if moment['m00'] != 0:
         x,y = int(moment['m10']/moment['m00']), int(moment['m01']/moment['m00'])
-        cv2.circle(blue,(x,y),50,255,-1)
-    
+        cv2.circle(blue,(int(cam.get(3))-x,y),50,255,-1)
+
     if moment1['m00'] != 0:
         x,y = int(moment1['m10']/moment1['m00']), int(moment1['m01']/moment1['m00'])
-        cv2.circle (green,(x,y),50,255,-1)
+        cv2.circle (green,(int(cam.get(3))-x,y),50,255,-1)
     
     cv2.imshow('video',frame)
     cv2.imshow('only blue',blue)
